08dd0c50f9b9b440d215e22811b94799f430ea75
Pablo Velasco <pablovelas04@gmail.com>
2021-11-25 22:41:43 +0100

    Se agrega el proyecto

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int(A);
+    int(D);
+    int(M);
+    printf("Escribe tu edad:");
+    scanf("%d" ,&A);
+    D=365*A;
+    M=12*A;
+    printf("Tu edad en dias es de %d dias",D,A);
+    printf(" Tu edad en meses es de %d meses", M,A);
+    return 0;
+}
